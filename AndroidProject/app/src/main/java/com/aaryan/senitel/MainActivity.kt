18d2f315c07cf275a3bc9483b95ddd0b19f5106f
aaryan765 <aaryannaik007@gmail.com>
2026-07-11 19:15:41 +0530

    Added Boot Screen

--- AndroidProject/app/src/main/java/com/aaryan/senitel/MainActivity.kt
+++ AndroidProject/app/src/main/java/com/aaryan/senitel/MainActivity.kt
@@ -5,43 +5,34 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.aaryan.senitel.screens.BootScreen
 import com.aaryan.senitel.ui.theme.SenitelTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             SenitelTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    BootScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BootScreenPreview() {
     SenitelTheme {
-        Greeting("Android")
+        BootScreen()
     }
 }
--- AndroidProject/app/src/main/java/com/aaryan/senitel/screens/BootScreen.kt
+++ AndroidProject/app/src/main/java/com/aaryan/senitel/screens/BootScreen.kt
@@ -1,2 +1,43 @@
 package com.aaryan.senitel.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun BootScreen() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+
+        Text(
+            text = "SENITEL",
+            color = Color.White,
+            fontSize = 36.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Text(
+            text = "Initializing...",
+            color = Color.Gray,
+            fontSize = 16.sp
+        )
+
+    }
+
+}
